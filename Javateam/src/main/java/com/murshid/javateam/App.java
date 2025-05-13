3d668d9a8a34d58efb98d55ae57cb8c8ab65818d
shivananda76 <mrshivahb@gmail.com>
2025-05-13 14:57:40 +0530

    first commit

---
+++ Javateam/src/main/java/com/murshid/javateam/App.java
@@ -0,0 +1,13 @@
+package com.murshid.javateam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
